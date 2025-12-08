5494d3755db2a69b6f889a4a8a5b70436da6f309
Joseph Monroe <joeqmonroe@gmail.com>
2025-12-08 17:59:57 -0600

    game loop created

--- src/Project.py
+++ src/Project.py
@@ -14,4 +14,22 @@ WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VELOCITY = 5
 
-window
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break 
+    pygame.quit()
+    quit()
+    
+
+if __name__ == "__main__":
+    main(window)
