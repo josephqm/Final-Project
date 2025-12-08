802fe19078a28ae2d944cfbc6f0cff0bf1c42482
Joseph Monroe <joeqmonroe@gmail.com>
2025-12-08 17:52:06 -0600

    global variables set

--- src/Project.py
+++ src/Project.py
@@ -6,4 +6,12 @@ from os import listdir
 from os.path import isfile, join 
 pygame.init()
 
-pygame.dispaly.set_caption("Final Project Joseph Monroe")
+pygame.display.set_caption("Final Project Joseph Monroe")
+
+BACKGROUND_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 1000, 800
+
+FPS = 60
+PLAYER_VELOCITY = 5
+
+window
