9bcbeb1e134abef1f32269370312fa73e83d6ae4
Joseph Monroe <joeqmonroe@gmail.com>
2025-12-08 18:14:31 -0600

    background established

--- src/Project.py
+++ src/Project.py
@@ -8,7 +8,7 @@ pygame.init()
 
 pygame.display.set_caption("Final Project Joseph Monroe")
 
-BACKGROUND_COLOR = (255, 255, 255)
+
 WIDTH, HEIGHT = 1000, 800
 
 FPS = 60
@@ -16,8 +16,29 @@ PLAYER_VELOCITY = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name):
+     image = pygame.image.load(join("assets", "Background", name))
+     _, _, width, height = image.get_rect()
+     tiles = []
+
+     for i in range(WIDTH // width + 1):
+         for j in range(HEIGHT // height + 1):
+             pos = (i * width, j * height)
+             tiles.append(pos)
+        
+     return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+
+
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Gray.png")
 
     run = True
     while run:
@@ -27,6 +48,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break 
+            
+        draw(window, background, bg_image)
     pygame.quit()
     quit()
     
