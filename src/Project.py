accafe01712615964c29b421eeb4ad986996ac4c
Joseph Monroe <joeqmonroe@gmail.com>
2025-12-08 18:29:22 -0600

    player class made

--- src/Project.py
+++ src/Project.py
@@ -16,6 +16,40 @@ PLAYER_VELOCITY = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None 
+        self.direction = "left"
+        self.animation_count = 0
+    
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+    
+    def move_left(self, vel):
+        self.x_vel = -vel 
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0 
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0  
+    
+    def loop(self, fpd):
+        self.move(self.x_vel, self.y_vel)
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
+
+
 def get_background(name):
      image = pygame.image.load(join("assets", "Background", name))
      _, _, width, height = image.get_rect()
@@ -48,7 +82,7 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break 
-            
+
         draw(window, background, bg_image)
     pygame.quit()
     quit()
